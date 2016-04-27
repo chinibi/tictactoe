e48ade7847d5df8d9f093ca959631877811c7cbc
Trevor Pham <trevor.dt.pham@gmail.com>
2016-04-27 07:22:18 -0700

    add semicolons

--- better_script.js
+++ better_script.js
@@ -1,5 +1,5 @@
-var letters = ['a','b','c','d','e','f','g','h','i']
-var ids = document.getElementsByClassName('square')
+var letters = ['a','b','c','d','e','f','g','h','i'];
+var ids = document.getElementsByClassName('square');
 var board;
 var winner;
 var playerTurn;
@@ -7,29 +7,29 @@ var winCombos = [
   '012', '345', '678', // rows
   '036', '147', '258', // columns
   '048', '246'         // diagonals
-]
+];
 
 // clear the board, X to move first
 function init() {
-  board = {}
+  board = {};
   for (var i=0; i<letters.length; i++) {
-    board[letters[i]] = ids[i].innerHTML = null
-  }
-  winner = ''
+    board[letters[i]] = ids[i].innerHTML = null;
+  };
+  winner = '';
   playerTurn = 'X';
-  document.getElementById('playerTurn').innerHTML = `${playerTurn} to move`
+  document.getElementById('playerTurn').innerHTML = `${playerTurn} to move`;
 }
 // start the game on load
-init()
+init();
 
 // reset button
-document.getElementById('reset').addEventListener('click', init)
+document.getElementById('reset').addEventListener('click', init);
 
 
 // game functions
 function nextTurn() {
-  playerTurn = (playerTurn == 'X' ? 'O' : 'X')
-  document.getElementById('playerTurn').innerHTML = `${playerTurn} to move`
+  playerTurn = (playerTurn == 'X' ? 'O' : 'X');
+  document.getElementById('playerTurn').innerHTML = `${playerTurn} to move`;
 }
 
 
@@ -42,11 +42,11 @@ function didThreeMatch(r,s,t) {
 
 // checks if all squares are taken
 function isTie() {
-  var allOccupied = true
+  var allOccupied = true;
   for (var key in board) {
-    if (!board[key]) allOccupied = false
+    if (!board[key]) allOccupied = false;
   }
-  return allOccupied
+  return allOccupied;
 }
 
 // checks each row, col, and diag for a winner
@@ -55,28 +55,28 @@ function isGameOver() {
   winCombos.forEach((combo) => {
     var r = board[letters[parseInt(combo[0])]],
         s = board[letters[parseInt(combo[1])]],
-        t = board[letters[parseInt(combo[2])]]
+        t = board[letters[parseInt(combo[2])]];
 
     if (didThreeMatch(r,s,t)) {
-      document.getElementById('playerTurn').innerHTML = `${playerTurn} has won`
-      playerTurn = null
-      over = true
+      document.getElementById('playerTurn').innerHTML = `${playerTurn} has won`;
+      playerTurn = null;
+      over = true;
     }
     else if (isTie()) {
-      document.getElementById('playerTurn').innerHTML = 'tie game'
-      playerTurn = null
-      over = true
+      document.getElementById('playerTurn').innerHTML = 'tie game';
+      playerTurn = null;
+      over = true;
     }
   })
-  return over
+  return over;
 }
 
 // make a move if game isn't over and square is empty
 for (var i=0; i<ids.length; i++) {
-  ids[i].addEventListener('click', function() {
+  ids[i].addEventListener('click', () => {
     if (!board[this.id] && playerTurn) {
-      this.innerHTML = board[this.id] = playerTurn
-      if (!isGameOver()) nextTurn()
+      this.innerHTML = board[this.id] = playerTurn;
+      if (!isGameOver()) nextTurn();
     }
   })
 }
